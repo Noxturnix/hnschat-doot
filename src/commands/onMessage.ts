c1caba78378769889de4a43c809b03ff60702596
Noxturnix <noxturnix@noxt.us>
2022-03-15 13:28:00 -0700

    removing log codes

--- src/commands/onMessage.ts
+++ src/commands/onMessage.ts
@@ -2,13 +2,10 @@ import { HnsChatMessageData } from "../interfaces/hnsChatMessageData";
 import { sedMessage } from "../libs/hnsChat";
 
 export default (messageData: HnsChatMessageData) => {
-  console.log("user:", messageData.user);
-  console.log("DOMAIN_ID:", process.env.DOMAIN_ID);
   if (
     !messageData.message.trim().toLowerCase().startsWith("s/") &&
     messageData.user !== process.env.DOMAIN_ID
   ) {
     sedMessage.set(messageData.conversation, messageData.message);
   }
-  console.log("sedMessage:", sedMessage);
 };
--- src/events/onMessage.ts
+++ src/events/onMessage.ts
@@ -7,9 +7,6 @@ export default (event: MessageEvent) => {
   let [command, ...splitedData] = event.data.toString().split(" ");
   let data = splitedData.join(" ").replace(/&amp;/g, "&");
 
-  console.log("command:", command);
-  console.log("data:", data);
-
   switch (command) {
     case "MESSAGE":
       let messageData: HnsChatMessageData = JSON.parse(data);
@@ -20,8 +17,6 @@ export default (event: MessageEvent) => {
       dootCommandLoop: for (let i = 0; i < dootCommands.length; i++) {
         let dootCommand = dootCommands[i];
 
-        console.log("dootCommand:", dootCommand);
-
         for (let j = 0; j < dootCommand.command.length; j++) {
           let trimmedcheckingCommand = dootCommand.command[j].trim();
           let checkingCommand =
@@ -31,9 +26,6 @@ export default (event: MessageEvent) => {
               : trimmedcheckingCommand.toLowerCase());
           let checkingMessage =
             dootCommand.ignoreCase === false ? trimmedMessage : trimmedMessage.toLowerCase();
-
-          console.log("checkingCommand:", checkingCommand);
-          console.log("checkingMessage:", checkingMessage);
 
           if (checkingCommand === checkingMessage || checkingMessage.startsWith(checkingCommand)) {
             let commandArgs = trimmedMessage
